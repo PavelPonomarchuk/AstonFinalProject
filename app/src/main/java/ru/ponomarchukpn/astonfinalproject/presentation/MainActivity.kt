2e6fac81ed4186a6c7b0e52cbe741075ca5dd2c7
Pavel Ponomarchuk <ponomarchuk83@gmail.com>
2023-05-29 18:46:41 +0500

    Prepared project

--- app/src/main/java/ru/ponomarchukpn/astonfinalproject/presentation/MainActivity.kt
+++ app/src/main/java/ru/ponomarchukpn/astonfinalproject/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package ru.ponomarchukpn.astonfinalproject
+package ru.ponomarchukpn.astonfinalproject.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.ponomarchukpn.astonfinalproject.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
